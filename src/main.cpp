ebc2b26594804b0b409284bd4482448f41e0ad11
Santiago Manopella <santimanopella@gmail.com>
2017-11-15 21:05:43 -0300

    modulo ethernet

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <Ethernet.h>
 #include <Arduino.h>
 
+
+
+// ## constantes ##
+
+// lcd
 LiquidCrystal_I2C lcd(0x3f, 20, 4);
 
+// ethernet
+byte mac[] = { 0xFE, 0xAD, 0xFE, 0xAF, 0xFE, 0xED };
+char server[] = "192.168.3.57";
+IPAddress ip(192, 168, 3, 58);
+EthernetClient client;
+
+
+// modos
 enum modo
 {
   TEST,
@@ -11,18 +26,28 @@ enum modo
   MANTENIMIENTO
 } modo;
 
+// sonidos
 enum sonido
 {
   PIANO,
   BAJO
 } sonido;
 
- 
+// request
+enum request
+{
+  GET,
+  POST
+} request;
+
+// notas
 const int keys[8] = { 22, 24, 26, 28, 30, 32, 34, 36 };
 const int threshold[8] = { 2, 2, 2, 2, 2, 2, 2, 2 };
-
 bool touched[8];
 
+// ## funciones ##
+
+// leer pin capacitivo
 uint8_t readCapacitivePin(int pinToMeasure) {
   volatile uint8_t* port;
   volatile uint8_t* ddr;
@@ -79,13 +104,13 @@ uint8_t readCapacitivePin(int pinToMeasure) {
   return cycles;
 }
 
+// manejar apretado pin
 void handleKey(int index) {
   int cycles = readCapacitivePin(keys[index]);
 
-  if (!touched[index] && cycles >= threshold[index]) {
+  if (cycles >= threshold[index]) {
     touched[index] = true;
-    Serial.print("Index: ");
-    Serial.println(index);
+    Serial.print(index);
   }
 
   if (touched[index] && cycles < threshold[index]) {
@@ -93,29 +118,67 @@ void handleKey(int index) {
   }
 }
 
-void setup()
+// 
+int mandarRequestHttp(int tipoRequest, char parametro[]) 
 {
-  // serial: datos
-  Serial.begin(9600);
 
-  // serial1: bluetooth
-  Serial1.begin(9600);
+  char request[1024];
+  switch(tipoRequest) {
+    case GET:
+    strcpy(request,"GET ");
+    break;
+  case POST:
+    strcpy(request,"POST ");
+    break;
+  }
 
-  // init lcd
-  lcd.init();
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Bienvenido!");
+  strcat(request, parametro);
+  strcat(request," HTTP/1.1");
+
+  if (client.connect(server, 8000)) {
+    Serial.println("connected");
+    client.println(request);
+    client.println("Host: 192.168.3.57:8000");
+    client.println("Connection: close");
+    client.println();
+  } else {
+    Serial.println("connection failed");
+  }
+
   delay(2000);
+  
+  while (client.available()) {
+    char c = client.read();
+    Serial.print(c);
+  }
 
-  // init modo
-  modo = NORMAL;
-  sonido = PIANO;
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+    client.stop();
+  }
 
-  imprimirLcd();
+  return 1;
 }
 
+// cambiar el tipo de sonido
+void cambiarTipoSonido()
+{
+
+  switch (sonido)
+  {
+  case PIANO:
+    mandarRequestHttp(GET,"/.vscode/arduino.json");
+    break;
+  case BAJO:
+    mandarRequestHttp(GET,"/.vscode/c_cpp_properties.json");
+    break;
+  }
+
+  return;
+}
+
+// imprimir el lcd
 void imprimirLcd()
 {
   switch (modo)
@@ -147,11 +210,7 @@ void imprimirLcd()
   }
 }
 
-void cambiarTipoSonido()
-{
-  return;
-}
-
+// leer el bluetooth
 void leerBluetooth()
 {
   if (Serial1.available())
@@ -165,11 +224,13 @@ void leerBluetooth()
       modo = MANTENIMIENTO;
     else if (lectura == 'P')
     {
+      Serial.println("cambio a piano");
       sonido = PIANO;
       cambiarTipoSonido();
     }
     else if (lectura == 'B')
     {
+      Serial.println("cambio a bajo");
       sonido = BAJO;
       cambiarTipoSonido();
     }
@@ -178,6 +239,38 @@ void leerBluetooth()
   }
 }
 
+// ## setup ##
+void setup()
+{
+  // serial: datos
+  Serial.begin(9600);
+
+  // serial1: bluetooth
+  Serial1.begin(9600);
+
+  // init ethern
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    Ethernet.begin(mac, ip);
+  }
+  delay(1000);
+
+  // init lcd
+  lcd.init();
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Bienvenido!");
+  delay(2000);
+
+  // init modo
+  modo = NORMAL;
+  sonido = PIANO;
+
+  imprimirLcd();
+}
+
+// # loop ## 
 void loop()
 {
   leerBluetooth();
@@ -195,19 +288,9 @@ void loop()
     break;
   }
 
-  switch (sonido)
-  {
-  case PIANO:
-    // Serial.println("PIANO");
-    break;
-  case BAJO:
-    // Serial.println("BAJO");
-    break;
-  }
-
   for (int i = 0; i < 8; i++) {
     handleKey(i);
   }
 
-  delay(50);
+  delay(100);
 }
